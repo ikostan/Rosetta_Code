956f1a4eda583fffdd906e9e8f73d3cab1266b36
ikostan <superegorius@gmail.com>
2017-05-07 01:29:18 -0700

    Two_D_sliding_block_puzzle game sceleton

--- src/Two_D_sliding_block_puzzle.java
+++ src/Two_D_sliding_block_puzzle.java
@@ -1,5 +1,11 @@
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+import javax.swing.JButton;
+import java.awt.Color;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 
-public class Two_D_sliding_block_puzzle implements MainInterface{
+public class Two_D_sliding_block_puzzle extends JFrame implements MainInterface{
 
 	/*
 	
@@ -34,7 +40,7 @@ public class Two_D_sliding_block_puzzle implements MainInterface{
 	 */
 	
 	
-	private String taskName = "2D sliding block puzzle game (2048)";
+	private static String taskName = "2D sliding block puzzle game (2048)";
 	private String taskDescription = "Implement a 2D sliding block puzzle game where blocks with numbers are combined to add their values.\n" +
 	"The rules are that each turn the player must perform a valid move shifting all tiles in one direction\n"+
 	"(up, down, left or right). A move is valid when at least one tile can be moved in that direction.\n"+
@@ -64,12 +70,157 @@ public class Two_D_sliding_block_puzzle implements MainInterface{
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		new Two_D_sliding_block_puzzle();
+		
 	}
 
+	private final static int HEIGH = 4;
+	private final int WIDTH = 4;
+	private final static int blockSize = 90;
+	private final static int padding = blockSize / 2;
+	private final static int size = blockSize * HEIGH + padding * 2;
+	
+	private static JFrame framePuzzle;
+	private static JTextField textScore;
+	private static JTextField textBestScore;
+	
+	public Two_D_sliding_block_puzzle(){
+		
+		framePuzzle = new JFrame();
+
+		setFrame();
+		createButtons();
+		setTextFields();
+		setLabels();
+		
+		framePuzzle.setVisible(true);
+	}
 	
 	
+	private static void setFrame(){
+		
+		framePuzzle.getContentPane().setBackground(new Color(222, 184, 135));
+		framePuzzle.getContentPane().setLayout(null);
+		framePuzzle.setTitle(taskName);
+		framePuzzle.setSize(size - 10, size + padding * 2);
+		framePuzzle.setResizable(false);
+		framePuzzle.setDefaultCloseOperation(WindowConstants.HIDE_ON_CLOSE);
+	}
 	
+	private static void createButtons(){
+		
+		JButton button_4 = new JButton("");
+		button_4.setBackground(new Color(255, 240, 245));
+		button_4.setBounds(322, 45, blockSize, blockSize);
+		framePuzzle.getContentPane().add(button_4);
+		
+		JButton button_2 = new JButton("");
+		button_2.setBackground(new Color(255, 240, 245));
+		button_2.setBounds(122, 45, 90, 90);
+		framePuzzle.getContentPane().add(button_2);
+		
+		JButton button_3 = new JButton("");
+		button_3.setBackground(new Color(255, 240, 245));
+		button_3.setBounds(222, 45, 90, 90);
+		framePuzzle.getContentPane().add(button_3);
+		
+		JButton button_1 = new JButton("");
+		button_1.setBackground(new Color(255, 240, 245));
+		button_1.setBounds(padding/2, padding, 90, 90);
+		framePuzzle.getContentPane().add(button_1);
+		
+		JButton button_5 = new JButton("");
+		button_5.setBackground(new Color(255, 240, 245));
+		button_5.setBounds(22, 146, 90, 90);
+		framePuzzle.getContentPane().add(button_5);
+		
+		JButton button_11 = new JButton("");
+		button_11.setBackground(new Color(255, 240, 245));
+		button_11.setBounds(222, 247, 90, 90);
+		framePuzzle.getContentPane().add(button_11);
+		
+		JButton button_7 = new JButton("");
+		button_7.setBackground(new Color(255, 240, 245));
+		button_7.setBounds(222, 146, 90, 90);
+		framePuzzle.getContentPane().add(button_7);
+		
+		JButton button_6 = new JButton("");
+		button_6.setBackground(new Color(255, 240, 245));
+		button_6.setBounds(122, 146, 90, 90);
+		framePuzzle.getContentPane().add(button_6);
+		
+		JButton button_8 = new JButton("");
+		button_8.setBackground(new Color(255, 240, 245));
+		button_8.setBounds(322, 146, 90, 90);
+		framePuzzle.getContentPane().add(button_8);
+		
+		JButton button_14 = new JButton("");
+		button_14.setBackground(new Color(255, 240, 245));
+		button_14.setBounds(122, 348, 90, 90);
+		framePuzzle.getContentPane().add(button_14);
+		
+		JButton button_9 = new JButton("");
+		button_9.setBackground(new Color(255, 240, 245));
+		button_9.setBounds(22, 247, 90, 90);
+		framePuzzle.getContentPane().add(button_9);
+		
+		JButton button_10 = new JButton("");
+		button_10.setBackground(new Color(255, 240, 245));
+		button_10.setBounds(122, 247, 90, 90);
+		framePuzzle.getContentPane().add(button_10);
+		
+		JButton button_15 = new JButton("");
+		button_15.setBackground(new Color(255, 240, 245));
+		button_15.setBounds(222, 348, 90, 90);
+		framePuzzle.getContentPane().add(button_15);
+		
+		JButton button_12 = new JButton("");
+		button_12.setBackground(new Color(255, 240, 245));
+		button_12.setBounds(322, 247, 90, 90);
+		framePuzzle.getContentPane().add(button_12);
+		
+		JButton button_13 = new JButton("");
+		button_13.setBackground(new Color(255, 240, 245));
+		button_13.setBounds(22, 348, 90, 90);
+		framePuzzle.getContentPane().add(button_13);
+		
+		JButton button_16 = new JButton("");
+		button_16.setBackground(new Color(255, 240, 245));
+		button_16.setBounds(322, 348, 90, 90);
+		framePuzzle.getContentPane().add(button_16);
+		
+		JButton btnRestart = new JButton("Restart the game");
+		btnRestart.setBounds(123, 462, 189, 23);
+		framePuzzle.getContentPane().add(btnRestart);
+	}
+	
+	private static void setTextFields(){
+		
+		textScore = new JTextField();
+		textScore.setEditable(false);
+		textScore.setBounds(63, 8, 86, 20);
+		framePuzzle.getContentPane().add(textScore);
+		textScore.setColumns(10);
+		
+		textBestScore = new JTextField();
+		textBestScore.setEditable(false);
+		textBestScore.setColumns(10);
+		textBestScore.setBounds(299, 8, 86, 20);
+		framePuzzle.getContentPane().add(textBestScore);
+	}
+	
+	
+	private static void setLabels(){
+		
+		JLabel lblScore = new JLabel("Score:");
+		lblScore.setBounds(22, 11, 46, 14);
+		framePuzzle.getContentPane().add(lblScore);
+		
+		JLabel lblBestScore = new JLabel("Best score:");
+		lblBestScore.setBounds(224, 11, 65, 14);
+		framePuzzle.getContentPane().add(lblBestScore);
+	}
 	
 	@Override
 	public String getTaskName() {
@@ -101,5 +252,4 @@ public class Two_D_sliding_block_puzzle implements MainInterface{
 		// TODO Auto-generated method stub
 		
 	}
-
 }
